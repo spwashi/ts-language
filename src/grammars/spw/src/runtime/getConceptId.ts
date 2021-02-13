9be1240c1f49451c1ca28da5f00b4ba19a4bba03
Sam Washington <sam@spwashi.com>
2021-02-12 23:34:08 -0600

    updated getConceptId

--- src/grammars/spw/src/runtime/getConceptId.ts
+++ src/grammars/spw/src/runtime/getConceptId.ts
@@ -1,13 +1,15 @@
+function toDomain(curr: string) {
+    return `{_${curr.indexOf(' ') > -1 ? `<${curr}>` : curr}}`;
+}
+
 export function getConceptId(domain: string, ...label: string[]) {
     return (
-        [domain, ...label]
-            .reduce(
-                (prev, curr, i, arr) => {
-                    if (i === arr.length - 1) {
-                        return `${prev}#${curr}`;
-                    }
-                    return `${prev ? prev + '#' : ''}{_${curr.indexOf(' ') > -1 ? `<${curr}>` : curr}}`
-                },
-            )
+        label.reduce((prev, curr, i, arr) => {
+                         if (i && (i === arr.length - 1)) return `${prev}#${curr}`;
+
+                         const currentDomain = toDomain(curr);
+                         return `${prev ? prev + '#' : ''}${currentDomain}`
+                     },
+                     toDomain(domain))
     );
 }
